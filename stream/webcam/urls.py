b9cc4789dada7c9528e28c7fcab3c573b063ab3d
소정 <sojeong@sojeong-ui-MacBookPro.local>
2023-04-11 02:40:14 +0900

    third ver

--- stream/webcam/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-
-from webcam import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('video_feed/', views.video_feed, name='video_feed')
-]
